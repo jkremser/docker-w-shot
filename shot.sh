dabe9589ba74215cf205e55371308c8543c85917
Jirka Kremser <jkremser@redhat.com>
2017-11-01 16:16:10 +0100

    init

---
+++ shot.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo -e "\n\nSmile I am taking a photo...\n\n"
+/usr/bin/ffmpeg -f video4linux2 -s 1280x960 -i /dev/video0 \
+  -q:v 0 -b:v 10000k -vcodec mjpeg -vframes: 1 \
+  /tmp/cam-data/pic-`date -u +'%Y-%m-%d-%H-%M-%S'`.jpg -y &> /dev/null
